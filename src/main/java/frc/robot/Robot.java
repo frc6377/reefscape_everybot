b1fbfae3af8926a5aef2a05f253ef3b4c61ca473
pranavr72 <pranavr@howdybots.org>
2025-03-09 16:41:28 -0500

    drivebase switching

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -69,7 +69,6 @@ public class Robot extends LoggedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-    m_robotContainer.configureBindings();
   }
 
   @Override
--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,12 +4,6 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.CANDriveSubsystem;
-
 public final class Autos {
   // Example autonomous command which drives forward for 1 second.
-  public static final Command exampleAuto(CANDriveSubsystem driveSubsystem) {
-    return driveSubsystem.arcadeDrive(() -> 0.5, () -> 0.0).withTimeout(1.0);
-  }
 }
